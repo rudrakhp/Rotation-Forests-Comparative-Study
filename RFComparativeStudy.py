ead33c46f81fd770ecefd097fc9055ad4e0279e0
rudrakh97 <rudrakh97@gmail.com>
2018-04-11 23:52:30 +0530

    Updated confidence of classifiers

--- RFComparativeStudy.py
+++ RFComparativeStudy.py
@@ -11,9 +11,7 @@ from sklearn.metrics import accuracy_score
 
 """
 Rotation forest:
-
 Reference - http://ieeexplore.ieee.org/ielx5/34/35279/01677518.pdf
-
 n denotes the ID of the *.data file
 Eg: "python3/python RFComparativeStudy.py 5" takes input from 5.data file in uci-datasets
 """
@@ -25,6 +23,20 @@ rotmat = []
 std = []
 med = []
 noise = []
+selected_subsets = []
+
+def takeInput(n):
+    path = './uci-datasets/' + str(n) + '.data'
+    data = np.genfromtxt(path, delimiter=",")
+    rows, cols = data.shape
+    Y = data[:,0]
+    X = data[:,1:rows]
+    return X,Y
+
+X, Y = takeInput(n)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+n_samps, n_features = X.shape
+n_classes = len(np.unique(X[0]))
 
 def train(X,Y):
         n_samps, n_features = X.shape
@@ -34,8 +46,7 @@ def train(X,Y):
         # X_z = (X-med)/(std+noise)
         X_z = (X-med)/std
         for i in range(n_classifiers):
-            # K = int(round(1 + n_features/4*random.random()))
-            K = 2
+            K = int(round(1 + n_features/4*random.random()))
             # K random subsets of the feature set
             k_features = np.zeros((K,n_features))
             for j in range(K):
@@ -47,9 +58,12 @@ def train(X,Y):
                 k_features[j,v] = 1
             R = np.zeros((n_features,n_features))
             for l in range(K):
-                # return indices of k_features that are non-zero = selected features
+                # return indices of k_features that are non-zero
                 pos = np.nonzero(k_features[l,:])[0]
+                # print pos
+                # selected features
                 vpos = [pos[m] for m in range(0, len(pos))]
+                # print vpos
                 # input modified according to selected features
                 X_zij = X_z[:, vpos]
                 # apply PCA
@@ -62,29 +76,26 @@ def train(X,Y):
             Xrot = X_z.dot(R)
             cl = DecisionTreeClassifier()
             cl.fit(Xrot, Y)
+            # print Xrot.shape[1]
             classifiers.append(cl)
-            return classifiers, rotmat, std, med, noise
+            # selected_subsets.append(v);
+        return classifiers, rotmat, std, med, noise
 
 def test(X):
     dim = len(classifiers)
-    out = np.zeros((len(X),dim))    
+    out = np.zeros((X.shape[0], n_classes))    
     X = (X-med)/std
+    # print X.shape
     for i in range(0,dim):
         xrot_z = X.dot(rotmat[i])
-        out[:,i] = classifiers[i].predict(xrot_z)
-    y_pred = mode(out, axis=1)[0]
+        mat = classifiers[i].predict_proba(xrot_z)
+        out = out + mat
+    # print out
+    y_pred = np.zeros(X.shape[0])
+    for i in range(X.shape[0]):
+        y_pred[i] = 1 + int(out[i].argmax())
+    # y_pred = mode(out, axis=1)[0]
     return y_pred
-
-def takeInput(n):
-    path = './uci-datasets/' + str(n) + '.data'
-    data = np.genfromtxt(path, delimiter=",")
-    rows, cols = data.shape
-    Y = data[:,0]
-    X = data[:,1:rows]
-    return X,Y
-
-X, Y = takeInput(n)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
 classifiers, rotmat, std, med, noise = train(X_train, Y_train)
 pred = test(X_test)
@@ -95,7 +106,6 @@ print ("Accuracy (Rotation Forest): " + str(100*accuracy_score(Y_test, pred)) + 
 
 """
 Boosting algorithm:
-
 Reference - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.AdaBoostClassifier.html#sklearn.ensemble.AdaBoostClassifier
 """
 boost = AdaBoostClassifier(base_estimator=DecisionTreeClassifier(), n_estimators=35, learning_rate=0.1, algorithm='SAMME.R', random_state=None)
@@ -104,7 +114,6 @@ print ("Accuracy (Boosting Algorithm): " + str(100*accuracy_score(Y_test, boost.
 
 """
 Bagging algorithm:
-
 Reference - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.BaggingClassifier.html#sklearn.ensemble.BaggingClassifier
 """
 bag = BaggingClassifier(base_estimator=DecisionTreeClassifier(), n_estimators=35, max_samples=1.0, max_features=1.0, bootstrap=True, bootstrap_features=False, oob_score=False, warm_start=False, n_jobs=1, random_state=None, verbose=0)
@@ -113,7 +122,6 @@ print ("Accuracy (Bagging Algorithm): " + str(100*accuracy_score(Y_test, bag.pre
 
 """
 Random Forest
-
 Reference - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
 """
 rf = RandomForestClassifier(n_estimators=35, criterion='gini', max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, bootstrap=True, oob_score=False, n_jobs=1, random_state=None, verbose=0, warm_start=False, class_weight=None)
