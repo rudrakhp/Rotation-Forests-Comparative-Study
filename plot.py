c9eff650c2aa5ed2bd352a6d2dc9a7d098877805
rudrakh97 <rudrakh97@gmail.com>
2018-04-15 13:46:11 +0530

    Debug performance plot

--- plot.py
+++ plot.py
@@ -29,13 +29,22 @@ for i in range(1, maxEnsemble+1):
             a[3] += 1.0
         k += 4
     x.append(i)
-    y1.append((100.0)*(float(a[0])/15.0))
-    y2.append((100.0)*(float(a[1])/15.0))
-    y3.append((100.0)*(float(a[2])/15.0))
-    y4.append((100.0)*(float(a[3])/15.0))
-l1, = plt.plot(x, y1, label='Rot Forest');
-l2, = plt.plot(x, y2, label = 'AdaBoost');
-l3, = plt.plot(x, y3, label = 'Bagging');
-l4, = plt.plot(x, y4, label = 'Random Forest');
-plt.legend(handles = [l1, l2, l3, l4])
+    y1.append((100.0)*(float(a[1])/15.0))
+    y2.append((100.0)*(float(a[2])/15.0))
+    y3.append((100.0)*(float(a[3])/15.0))
+areaLabels = ['Rot Forest', 'AdaBoost', 'Bagging', 'Random Forest']
+fig, ax = plt.subplots()
+ax.stackplot(x, y1, y2, y3)
+loc = y1.index(max(y1))
+ax.text(loc+2, y1[loc]*0.25, areaLabels[1])
+loc = y2.index(max(y2))
+ax.text(loc, y1[loc] + y2[loc]*0.33, areaLabels[2])
+loc = y3.index(max(y3))
+ax.text(loc, y1[loc] + y2[loc] + y3[loc]*0.5, areaLabels[3])
+loc =y3.index(min(y3))
+ax.text(loc, (y1[loc] + y2[loc] + y3[loc])*1.75, areaLabels[0]) 
+ax.set_xlabel("Number of classifiers in ensemble")
+axes = plt.gca()
+axes.set_xlim(1,35)
+axes.set_ylim(0,100)
 plt.show()
